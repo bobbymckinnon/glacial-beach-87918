ed7effacc7e439671278d0b64be77e66b1b3672d
bobbymckinnon <bobby.mckinnon@simplesurance.de>
2018-09-08 13:39:25 +0200

    test

--- src/AppBundle/Service/Guide/GuideFilter.php
+++ src/AppBundle/Service/Guide/GuideFilter.php
@@ -8,8 +8,8 @@ use AppBundle\Service\GuideAPIInterface;
 
 class GuideFilter implements FilterInterface
 {
-    const DAY_MAX_MIN       = 750;
-    const RELOCATION_MIN    = 30;
+    const DAY_MAX_MIN = 750;
+    const RELOCATION_MIN = 30;
 
     /**
      * @var GuideAPIInterface
@@ -31,22 +31,29 @@ class GuideFilter implements FilterInterface
      */
     public function handle(array $options): array
     {
+        $e = [];
         $result = [];
         $data = $this->guideApi->fetch();
         $budget = 0;
-        $days = $options['days']?:1;
+        $days = $options['days'] ?: 1;
 
         for ($i = 1; $i <= $days; ++$i) {
             $cTime = 0;
             $start = new \DateTime('tomorrow');
             $start->setTime(10, 00);
             $start->add(new \DateInterval('P' . $i . 'D'));
+            if (isset($result['schedule'])) {
+                foreach ($result['schedule'] as $key) {
+                    $e = array_merge($e, array_values(array_column($key, 'id')));
+                }
+            }
 
             foreach ($data as $item) {
                 if ((self::DAY_MAX_MIN >= $cTime &&
                     self::DAY_MAX_MIN >= $cTime + ($item['duration'] + 30)) &&
                     !isset($result['schedule'][$i][$item['id']]) &&
-                    $options['budget'] >= $budget + $item['price']
+                    $options['budget'] >= $budget + $item['price'] &&
+                    !\in_array($item['id'], $e, true)
                 ) {
                     $startT = clone $start;
                     $startT->add(new \DateInterval('PT' . $cTime . 'M'));
@@ -54,10 +61,11 @@ class GuideFilter implements FilterInterface
                     $result['schedule'][$i][] = [
                         'id' => $item['id'],
                         'start' => $startT->format('Y-m-d H:i'),
+                        'duration' => $item['duration'],
                     ];
                     $budget += $item['price'];
+                    $cTime = $cTime + ($item['duration'] + 30);
                 }
-                $cTime = $cTime + ($item['duration'] + 30);
             }
         }
         $totalActivities = array_values(array_map('count', $result['schedule']));
